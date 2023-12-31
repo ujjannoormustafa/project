4d35a585239cf15bf866ce2ba7db9933b3685bad
ujjannoormustafa <ujjannoormustafa123@gmail.com>
2024-01-01 01:38:45 +0500

    add taillwind

--- src/componet/home.jsx
+++ src/componet/home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <div> <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import about from "./componet/about";/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
